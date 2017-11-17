1a48ab75a0099a1f616715a24ef9fa969747fd42
Leonardo Val <leonardo.val@creatartis.com>
2017-11-17 12:53:12 -0300

    Exported submodules

--- src/__epilogue__.js
+++ src/__epilogue__.js
@@ -1,6 +1,16 @@
 // See __prologue__.js
-	exports.mancala = { //FIXME
+	exports.mancala = {
 		Mancala: that['ludorum-game-mancala'].Mancala
+	};
+	exports.colograph = {
+		Colograph: that['ludorum-game-colograph'].Colograph
+	};
+	exports.connect4 = {
+		ConnectFour: that['ludorum-game-connect4'].ConnectFour
+	};
+	exports.reversi = {
+		Reversi: that['ludorum-game-reversi'].Reversi,
+		Othello: that['ludorum-game-reversi'].Othello
 	};
 
 	return exports;
